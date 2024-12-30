9eb63f7dbda92efd0d569ceeb13493df2bc30f06
keson2625 <wjdgns2625@naver.com>
2024-12-30 17:30:44 +0900

    자바 01일차 : 비교연습 예제

---
+++ java_sjh/src/day01/Ex12_Operator_Comparison.java
@@ -0,0 +1,25 @@
+package day01;
+
+public class Ex12_Operator_Comparison {
+
+	public static void main(String[] args) {
+		//비교 연산자 예제 : 결과값이 false 아니면 true
+		int num1 = 1, num2 =2;
+		System.out.println(num1 + ">" + num2 + "? " + (num1>num2));
+		System.out.println(num1 + ">=" + num2 + "? " + (num1>=num2));
+		System.out.println(num1 + "<" + num2 + "? " + (num1<num2));
+		System.out.println(num1 + "<=" + num2 + "? " + (num1<=num2));
+		System.out.println(num1 + "==" + num2 + "? " + (num1==num2));
+		System.out.println(num1 + "!=" + num2 + "? " + (num1!=num2));
+	
+		String str1 = "abc";
+		String str2 = "abc";
+		String str3 = new String("abc");
+		System.out.println(str1 + "==" + str2 + "? " + (str1==str2));
+		System.out.println(str1 + "==" + str2 + "? " + (str1==str3));
+		
+		System.out.println("------------------------------");
+		System.out.println(str1 + " equals " + str2 + "? " + (str1.equals(str2)));
+		System.out.println(str1 + " equals " + str3 + "? " + (str1.equals(str3)));
+}
+}
