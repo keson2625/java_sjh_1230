db7383bb7710f5a5d1ad9c0e625cf9fa4d263d63
keson2625 <wjdgns2625@naver.com>
2024-12-30 17:30:15 +0900

    자바 01일차 : 변수연습 예제

---
+++ java_sjh/src/day01/Ex09_Variable_Test.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class Ex09_Variable_Test {
+
+	public static void main(String[] args) {
+		
+		/*학생의 국어, 영어, 수학 성적을 저장하기 위한 변수를 선언하세요*/
+		
+		int korScore, engScore, mathScore;
+
+	
+		/* 위에 성적의 평균을 저장하기 위한 변수를 선언.*/
+		double averge;
+		
+		/*학생의 학점을 저장하기 위한 변수선언. A,B,C,D,F만 저장*/
+		char result;
+		String result2;
+	}
+}
