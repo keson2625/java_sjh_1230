08c518b38faed1fc7560373b70bf3493aa9c14ff
keson2625 <wjdgns2625@naver.com>
2024-12-30 12:38:27 +0900

    자바 01일차

---
+++ java_sjh/src/day01/Ex01_Helloworld.java
@@ -0,0 +1,17 @@
+package day01;
+
+// 한줄 주석. //가 나온 뒤부터 주석
+/* 여러줄 주석. 이 사이에 있는 모든 글자들이 주석*/
+public class Ex01_Helloworld {
+	/**
+	 * 메소드를 설명할 떄 사용하는 주석입니다.
+	 * @param args 뭔지 몰름
+	 */
+	//프로그램이 실행되려면 main이 있어애한다.
+	public static void main(String[] args) {
+		// 들여쓰기는 탭		 					
+		// 내여쓰기는 쉬프트+탭
+		System.out.println("Hello World!");//Hello World!를 출력합니다.
+	}
+
+}
