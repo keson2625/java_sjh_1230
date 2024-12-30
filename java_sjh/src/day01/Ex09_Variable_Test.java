f6f3783d60f207bf9babb64187be2553a54b9ec2
keson2625 <wjdgns2625@naver.com>
2024-12-30 17:50:58 +0900

    자바 01차 : 변수연습 예제

--- java_sjh/src/day01/Ex09_Variable_Test.java
+++ java_sjh/src/day01/Ex09_Variable_Test.java
@@ -13,7 +13,18 @@ public class Ex09_Variable_Test {
 		double averge;
 		
 		/*학생의 학점을 저장하기 위한 변수선언. A,B,C,D,F만 저장*/
-		char result;
-		String result2;
+		char result1 = 'A';
+		System.out.println(result1);
+		char result2 = 'B';
+		System.out.println(result2);
+		char result3 = 'C';
+		System.out.println(result3);
+		char result4 = 'D';
+		System.out.println(result4);
+		String result5 = "F";
+		System.out.println(result5);
+	
+		
+		//String result1; 도 사용가능
 	}
 }
