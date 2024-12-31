fdbb0060a7e37f5063d00ca2816298e594b37df0
keson2625 <wjdgns2625@naver.com>
2024-12-31 11:00:25 +0900

    자바 01: home

--- java_sjh/src/day01/java01_home.java
+++ java_sjh/src/day01/java01_home.java
@@ -7,11 +7,14 @@ public class java01_home {
 		int count = 20, a = 30;
 
 		for (int kor = 70; count < a; count++)
+
+		{
 			if (kor >= 60)
 				System.out.println((kor + 1) + "번 학생은 합격입니다.");
 
 			else {
 				System.out.println((kor + 1) + "번 학생은 불합격입니다.");
 			}
+		}
 	}
 }
