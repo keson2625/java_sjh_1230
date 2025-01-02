6c2486a0f99946f61294392e54da92ed0f2f880b
keson2625 <wjdgns2625@naver.com>
2025-01-02 09:26:27 +0900

    자바 1일차 : 오버플로우 예제

--- java_sjh/src/day01/Ex04_Variable_Overflow.java
+++ java_sjh/src/day01/Ex04_Variable_Overflow.java
@@ -4,13 +4,13 @@ public class Ex04_Variable_Overflow {
 	
 	public static void main(String[] args) {
 		byte num1 = 127;
-		num1++;//num1를 1씩 증가 //오버 플로우
-		System.out.println(num1);//byte로 표현 가능한 가장 큰 숫자에 1을 더하면 127이 아닌 -128이 나옴
+		num1++;//num1에 1을 증가
+		//오버 플로우
+		System.out.println(num1);//byte로 표현 가능한 가장 큰 숫자에 1을 더하면 128이 아닌 -128이 나옴.
 		
-		byte num2 = 127;
-		num1--;//num2를 1씩 감소 //언더 플로우
-		System.out.println(num2);//byte로 표현 가능한 가장 작은 숫자에 1을 빼면 -128이 아닌 127이 나옴
-		
+		byte num2 = -128;
+		num2--;//num2에 1을 감소
+		//언더 플로우
+		System.out.println(num2);//byte로 표현 가능한 가장 작은 숫자에 1을 빼면 -129가 아닌 127이 나옴.
 	}
-
 }
