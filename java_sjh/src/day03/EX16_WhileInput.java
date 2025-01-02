30a9e5ee420282a011f87193ed080f3df6e70f15
keson2625 <wjdgns2625@naver.com>
2025-01-02 17:13:54 +0900

    자바 3일차 : while문 예제

---
+++ java_sjh/src/day03/EX16_WhileInput.java
@@ -0,0 +1,21 @@
+package day03;
+
+import java.util.Scanner;
+
+public class EX16_WhileInput {
+
+	public static void main(String[] args) {
+		/* 문장을 입력받아 문자가 q일 때 종료하는 코드를 작성 
+		 * 
+		 */
+		char ch = 'a';
+		
+		Scanner scan = new Scanner(System.in);	
+		
+		while(ch != 'q') {
+			System.out.print("입력 : ");
+			ch = scan.next().charAt(0);
+		}
+		System.out.println("프로그램 종료");
+	}
+}
