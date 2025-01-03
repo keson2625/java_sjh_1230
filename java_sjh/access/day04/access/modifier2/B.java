fc470e6362526810e980901ee276729a8fb88488
keson2625 <wjdgns2625@naver.com>
2025-01-03 17:22:45 +0900

    자바 4일차 : 접근 제어자 예제

---
+++ java_sjh/access/day04/access/modifier2/B.java
@@ -0,0 +1,17 @@
+package day04.access.modifier2;
+
+public class B {
+
+	public String name;
+	String address;
+	private String num;
+		
+	public void setNum(String num1) {
+		num = num1;
+	}
+	
+	public String getNum() {
+		return num;		
+	}
+}
+
