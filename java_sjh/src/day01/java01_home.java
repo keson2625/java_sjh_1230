49f72c4d439617d95365cdfe59813459a6d83e5f
keson2625 <wjdgns2625@naver.com>
2025-01-03 12:22:48 +0900

    자바 : 집연습

--- java_sjh/src/day01/java01_home.java
+++ java_sjh/src/day01/java01_home.java
@@ -1,10 +1,33 @@
 package day01;
 
+import java.util.Scanner;
+
 public class java01_home {
 
 	public static void main(String[] args) {
+	
+		/* 문장을 입력받아 문자가 q일 때 종료하는 코드를 작성 
+		 * 
+		 */
+
+		Scanner scan = new Scanner(System.in);
+
+		
+		for(;;) {
+			System.out.print("문장을 입력하세요 : ");
+			char a;
+			a= scan.next().charAt(0);
+			if(a=='q')
+			break;{
+				System.out.println("끝");
+			
+			}
+			
+		}
+		
 		
 	}
 }
+
 
 	
