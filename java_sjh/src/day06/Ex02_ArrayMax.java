d651f4ba75e128e20c23d83b5a4e2cc21ff53b76
keson2625 <wjdgns2625@naver.com>
2025-01-07 15:42:10 +0900

    자바 06일차 : 배열 예제

--- java_sjh/src/day06/Ex02_ArrayMax.java
+++ java_sjh/src/day06/Ex02_ArrayMax.java
@@ -4,7 +4,7 @@ public class Ex02_ArrayMax {
 
 	public static void main(String[] args) {
 		/* 배열에 저장된 값 중 가장 큰 값을 출력하는 코드를 작성하세요. */
-		int [] arr = new int[] {-1, -10, -9, -20, -3, -4};
+		int [] arr = new int[] {1, 10, 9, 20, 3, 4};
 
 		int max = arr[0];
 		
