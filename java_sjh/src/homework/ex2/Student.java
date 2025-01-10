24a200f4ed38dc05a4b9bd5a8db2dedb3b3a3e8f
keson2625 <wjdgns2625@naver.com>
2025-01-10 17:49:45 +0900

    과제

--- java_sjh/src/homework/ex2/Student.java
+++ java_sjh/src/homework/ex2/Student.java
@@ -1,5 +1,8 @@
 package homework.ex2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import lombok.Data;
 
 @Data
@@ -9,5 +12,22 @@ public class Student {
 	public String name;
 
 	//학생 개인의 성적을 담고 있는 배열
-	public Subject[] list;
+	ArrayList<Student> list = new ArrayList<>();
+	
+	 private static Student inputStudent(Scanner scan) {
+	        System.out.print("학년: ");
+	        int grade = scan.nextInt();
+	        System.out.print("반: ");
+	        int classNum = scan.nextInt();
+	        System.out.print("번호: ");
+	        int num = scan.nextInt();
+	        System.out.print("이름: ");
+	        String name = scan.next();
+	        System.out.print("과목: ");
+	        String subject = scan.next();
+	        System.out.print("성적: ");
+	        int score = scan.nextInt();
+	        return null;
+	 }
 }
+//public Subject[] list;
--- java_sjh/src/homework/ex2/Subject.java
+++ java_sjh/src/homework/ex2/Subject.java
@@ -1,9 +1,30 @@
 package homework.ex2;
 
+import java.util.Objects;
+
 public class Subject {
 
 	public int grade;
 	public int smester;
 	public String name;
 	public int score;
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Subject other = (Subject) obj;
+		return grade == other.grade && Objects.equals(name, other.name) && score == other.score
+				&& smester == other.smester;
+	}
+	
+	
 }
+	
+	
+
