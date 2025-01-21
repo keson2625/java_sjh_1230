596e1b5832131e5aef366685ac4bb67b5f180cf6
keson2625 <wjdgns2625@naver.com>
2025-01-21 17:12:51 +0900

    자바 14일차 :포스트 예제

--- java_sjh/src/day14/Ex01_Post.java
+++ java_sjh/src/day14/Ex01_Post.java
@@ -1,131 +1,220 @@
 package day14;
 
-import java.io.Reader;
-import java.security.DrbgParameters.NextBytes;
-import java.text.ParseException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import lombok.Data;
 
 public class Ex01_Post {
 
-	/*	게시글 프로그램을 위한 클래스를 선언하세요.
-	 * 	게시글 등록
-	 * 	- 제목, 내용, 작성자를 입력
-	 *	게시글 수정
-	 *	-게시글 번호를 선택
-	 *	- 제목, 내용을 수정
-	 *	게시글 삭제
-	 *	- 게시글 번호를 선택해서 삭제
-	 *	게시글 조회
-	 *	- 게시글 번호를 이요해서 조회하고 조회수 1을 증가
-	 */
-	
 	static Scanner scan = new Scanner(System.in);
-	static ArrayList<Post> list = new ArrayList<Post>();
+	static List<Post> list = new ArrayList<Post>();
 	
 	public static void main(String[] args) {
-		
+		/* 게시글 프로그램을 위한 클래스를 선언하세요.
+		 * 게시글 등록
+		 *  - 제목, 내용, 작성자를 입력
+		 * 게시글 수정
+		 * 	- 게시글 번호를 선택
+		 * 	- 제목, 내용을 수정
+		 * 게시글 삭제
+		 * 	- 게시글 번호를 선택해서 삭제
+		 * 게시글 조회
+		 * 	- 게시글 번호를 이용하여 조회하고 조회수 1 증가
+		 * */
 		int menu;
 		
+		String fileName = "src/day14/post.txt";
+		
+		list = (ArrayList<Post>)load(fileName);
+		
+		if(list == null || list.size() == 0) {
+			list = new ArrayList<Post>();
+		}
+		else {
+			int lastIndex = list.size()-1;
+			Post lastPost = list.get(lastIndex);
+			int lastNum = lastPost.getNum();
+			Post.setCount(lastNum + 1);
+		}
+		
 		do {
+			
 			printMenu();
+			
 			menu = scan.nextInt();
 			scan.nextLine();
+			System.out.println("--------------------");
 			
 			runMenu(menu);
 			
 		}while(menu != 5);
+		save(fileName, list);
 	}
-	private static void printMenu() {
-	
-		System.out.println("---------------------\r\n"
-							+"1. 게시글 등록\r\n"
-							+"2. 게시글 수정\r\n"
-							+"3. 게시글 삭제\r\n"
-							+"4. 게시글 조회\r\n"
-							+"5. 프로그램 종료\r\n"
-							+"---------------------"
-							);
-		System.out.print("메뉴 선택 : ");
+
+	private static void save(String fileName, Object obj) {
+		try(FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos)){
+			
+			oos.writeObject(obj);
+			
+		} catch (Exception e) {
+			System.out.println("-----------------");
+			System.out.println("저장하기 실패");
+			System.out.println("-----------------");
 		}
-	
+		
+	}
+	private static Object load(String fileName) {
+		try(FileInputStream fis = new FileInputStream(fileName);
+			ObjectInputStream ois = new ObjectInputStream(fis)){
+			
+			return ois.readObject();
+			
+		} catch (Exception e) {
+			System.out.println("-----------------");
+			System.out.println("불러오기 실패");
+			System.out.println("-----------------");
+		}
+		return null;
+	}
+
+	private static void printMenu() {
+		System.out.println("--------------------");
+		System.out.println("1. 게시글 등록");
+		System.out.println("2. 게시글 수정");
+		System.out.println("3. 게시글 삭제");
+		System.out.println("4. 게시글 조회");
+		System.out.println("5. 종료");
+		System.out.println("--------------------");
+		System.out.print("메뉴 선택 : ");
+	}
+
 	private static void runMenu(int menu) {
 		
-		
 		switch(menu) {
-					case 1 : insertPost();
-						break;
-					case 2 : updatePost();
-						break;
-					case 3 : deletePost();
-						break;
-					case 4 : searchPost();
-						break;
-					case 5 : System.out.println("프로그램을 종료합니다.");
-						break;
-					default: System.out.println("잘못된 입력입니다.");
-					}
+		case 1:
+			insert();
+			break;
+		case 2:
+			update();
+			break;
+		case 3:
+			delete();
+			break;
+		case 4:
+			view();
+			break;
+		case 5:
+			System.out.println("프로그램을 종료합니다.");
+			break;
+		default:
+			System.out.println("올바른 메뉴를 선택하세요.");
 		}
-	
-	private static void insertPost() {
-		Post po = inputPost();
 		
-		if(e.po) {
-			System.out.println("이미 등록된 학생입니다.");
-			return;
-		}
-		studentList.add(po);
-		System.out.println("학생을 등록했습니다.");
-	} catch (Exception e) {
-		System.out.println("잘못 입력했습니다.");
-	} 
+	}
 
-
-	
-
-	private static Post inputPost() throws ParseException {
+	private static void insert() {
 		
+		//제목, 내용, 작성자를 입력
 		System.out.print("제목 : ");
-		String titie = scan.nextLine();
-		
+		String title = scan.nextLine();
 		System.out.print("내용 : ");
 		String content = scan.nextLine();
-		
 		System.out.print("작성자 : ");
 		String writer = scan.nextLine();
 		
-		return new Post(titie, content, writer);
+		//입력받은 내용으로 객체를 생성
+		Post post = new Post(title, content, writer);
 		
-		
+		//목록에 추가
+		list.add(post);
+		System.out.println(list);
 	}
-		
-	private static void updatePost() {
-		// TODO Auto-generated method stub
-		
-	}
-	private static void deletePost() {
-		// TODO Auto-generated method stub
-		
-	}
-	private static void searchPost() {
-	
-	}
-}	
 
+	private static void update() {
+		
+		//번호를 입력
+		System.out.print("번호 : ");
+		int num = scan.nextInt();
+		scan.nextLine();
+		
+		//번호와 일치하는 게시글이 있는지 확인해서 없으면 알림 후 종료
+		//Objects.equals(o1, o2) => o1과 o2가 다른 클래스이면 무조건 false
+		int index = list.indexOf(new Post(num)); 
+		
+		if(index < 0) {
+			System.out.println("등록되지 않거나 삭제된 게시글입니다.");
+			return;
+		}
+		
+		//제목, 내용을 입력
+		System.out.print("제목 : ");
+		String title = scan.nextLine();
+		System.out.print("내용 : ");
+		String content = scan.nextLine();
+		
+		//목록에서 번호와 일치하는 게시글을 가져옴
+		Post post = list.get(index);
+		//가져온 게시글의 제목과 내용을 수정
+		post.setTitle(title);
+		post.setContent(content);
+	}
+
+	private static void delete() {
+		//번호를 입력
+		System.out.print("번호 : ");
+		int num = scan.nextInt();
+		scan.nextLine();
+		
+		if(list.remove(new Post(num))) {
+			System.out.println("게시글을 삭제 했습니다.");
+			return;
+		}
+		System.out.println("등록되지 않거나 삭제된 게시글입니다.");
+		
+	}
+
+	private static void view() {
+		//번호를 입력
+		System.out.print("번호 : ");
+		int num = scan.nextInt();
+		scan.nextLine();
+		
+		//번호와 일치하는 게시글이 있는지 확인해서 없으면 알림 후 종료
+		//Objects.equals(o1, o2) => o1과 o2가 다른 클래스이면 무조건 false
+		int index = list.indexOf(new Post(num)); 
+		
+		if(index < 0) {
+			System.out.println("등록되지 않거나 삭제된 게시글입니다.");
+			return;
+		}
+		//해당 게시글이 있으면 조회수 증가 후 출력
+		Post post = list.get(index);
+		post.view();
+		//post.setView(post.getView()+1);
+		post.print();
+		
+	}
+
+}
 
 @Data
-class Post{
+class Post implements Serializable{
+	
+	private static final long serialVersionUID = 7950467669298764591L;
 	
 	private static int count;
 	private int num;
-	private String titie, content, writer;
+	private String title, content, writer;
 	private Date date;
 	private int view;
 	
@@ -135,37 +224,51 @@ class Post{
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
+		if (getClass() != obj.getClass()) 
 			return false;
 		Post other = (Post) obj;
 		return num == other.num;
 	}
 
-	public Post(String titie, String content, String writer) {
+	public void view() {
+		view++;
+	}
+
+	public Post(String title, String content, String writer) {
 		num = ++count;
-		this.titie = titie;
+		this.title = title;
 		this.content = content;
 		this.writer = writer;
 		date = new Date();
 	}
 	
+	public Post(int num) {
+		this.num = num;
+	}
+
 	public void print() {
-		System.out.println("--------------------------");
+		System.out.println("------------------------");
 		System.out.println("번호 : " + num);
-		System.out.println("제목 : " + titie);
+		System.out.println("제목 : " + title);
 		System.out.println("내용 : " + content);
 		System.out.println("작성자 : " + writer);
-		System.out.println("작성일 : " + getDateStr());
+		System.out.println("작성일 : " + getDateStr() );
 		System.out.println("조회수 : " + view);
-		System.out.println("--------------------------");
-		
+		System.out.println("------------------------");
 	}
-	
+
 	private String getDateStr() {
 		//Date -> String
-		//yyyy--MM-dd HH:mm:ss
-		SimpleDateFormat format = new SimpleDateFormat("yyyy--MM-dd HH:mm:ss");
+		//yyyy-MM-dd HH:mm:ss
+		SimpleDateFormat format = 
+				new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return format.format(date);
-		
+	}
+	
+	public static int getCount() {
+		return count;
+	}
+	public static void setCount(int count) {
+		Post.count = count;
 	}
 }
