12c47bc5f4684cfc7c3c9ba5fe6b401d8ed40cd7
keson2625 <wjdgns2625@naver.com>
2025-04-01 12:21:34 +0900

    스프링

--- spring/framework/spring/src/main/java/kr/kh/spring/controller/CommentController.java
+++ spring/framework/spring/src/main/java/kr/kh/spring/controller/CommentController.java
@@ -1,24 +1,27 @@
 package kr.kh.spring.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.kh.spring.model.vo.CommentVO;
 import kr.kh.spring.model.vo.MemberVO;
 import kr.kh.spring.pagination.Criteria;
+import kr.kh.spring.pagination.PageMaker;
 import kr.kh.spring.service.CommentService;
 
 //컨트롤러 안의 모든 메소드에 @ResponseBody가 붙은 경우에 사용
-@RestController
+@Controller
 @RequestMapping("/comment")
 public class CommentController {
 
@@ -26,18 +29,42 @@ public class CommentController {
 	CommentService commentService;
 
 	@PostMapping("/insert")
+	@ResponseBody
 	public boolean insert(@RequestBody CommentVO comment, HttpSession session) {
 		MemberVO user = (MemberVO)session.getAttribute("user");
 		return commentService.insertComment(comment, user);
 	}
 	
+	@GetMapping("/list2")
+	public String list2(Model model, Criteria cri) {
+		
+		List<CommentVO> list= commentService.getCommentList(cri);
+		PageMaker pm = commentService.getPageMaker(cri);
+		model.addAttribute("list", list);
+		model.addAttribute("pm", pm);
+		return "/comment/list";
+	}
 	@PostMapping("/list")
-	public Map<String, Object> list(@RequestBody Criteria cri){
-		HashMap<String, Object> map = new HashMap <String, Object>();
-		List<CommentVO> list = commentService.getCommentList(cri);
-		
-		map.put("list", list);
-		
-		return map;
+	public String list(Model model,@RequestBody Criteria cri) {
+		cri.setPerPageNum(5);
+		List<CommentVO> list= commentService.getCommentList(cri);
+		PageMaker pm = commentService.getPageMaker(cri);
+		model.addAttribute("list", list);
+		model.addAttribute("pm", pm);
+		return "comment/list";
+	}
+	
+	@PostMapping("/delete")
+	@ResponseBody
+	public boolean delete(@RequestParam int co_num, HttpSession session) {
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		return commentService.deleteComment(co_num, user);
+	}
+	
+	@PostMapping("/update")
+	@ResponseBody
+	public boolean update(@RequestBody CommentVO comment, HttpSession session) {
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		return commentService.updateComment(comment, user);
 	}
 }
--- spring/framework/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
+++ spring/framework/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
@@ -9,16 +9,16 @@ import kr.kh.spring.dao.CommentDAO;
 import kr.kh.spring.model.vo.CommentVO;
 import kr.kh.spring.model.vo.MemberVO;
 import kr.kh.spring.pagination.Criteria;
-
+import kr.kh.spring.pagination.PageMaker;
 
 @Service
-public class CommentServiceImp implements CommentService{
+public class CommentServiceImp implements CommentService {
 
 	@Autowired
 	CommentDAO commentDao;
-	
+
 	@Override
-	public boolean insertComment(CommentVO comment, MemberVO user){
+	public boolean insertComment(CommentVO comment, MemberVO user) {
 		if(comment == null) {
 			return false;
 		}
@@ -41,5 +41,41 @@ public class CommentServiceImp implements CommentService{
 		}
 		return commentDao.selectCommentList(cri);
 	}
-}
 
+	@Override
+	public PageMaker getPageMaker(Criteria cri) {
+		if(cri == null) {
+			return null;
+		}
+		int totalCount = commentDao.selectCountCommentList(cri);
+		return new PageMaker(3, cri, totalCount);
+	}
+
+	@Override
+	public boolean deleteComment(int co_num, MemberVO user) {
+		if(user == null) {
+			return false;
+		}
+		//작성자 확인
+		CommentVO comment = commentDao.selectComment(co_num);
+		
+		if(comment == null || !comment.getCo_me_id().equals(user.getMe_id())) {
+			return false;
+		}
+		return commentDao.deleteComment(co_num);
+	}
+
+	@Override
+	public boolean updateComment(CommentVO comment, MemberVO user) {
+		if(comment == null || user == null) {
+			return false;
+		}
+		
+		CommentVO dbComment = commentDao.selectComment(comment.getCo_num());
+
+		if(dbComment == null || !dbComment.getCo_me_id().equals(user.getMe_id())) {
+			return false;
+		}
+		return commentDao.updateComment(comment);
+	}
+}
