a22fc804c857e74383fdbfbdddbbebd3530ea74a
keson2625 <wjdgns2625@naver.com>
2025-04-21 18:09:21 +0900

    스프링부트

---
+++ spring/boot/boot/src/main/java/model/vo/MemberVO.java
@@ -0,0 +1,15 @@
+package model.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+  
+  String me_id;
+  String me_pw;
+  String me_email;
+  String me_authority;
+  String me_cookie;
+  String me_limite;
+  
+}
